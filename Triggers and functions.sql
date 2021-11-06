619be681a258997df9067449315ea450287c575c
Mahbuba Sultana <87199449+msultana22@users.noreply.github.com>
2021-11-05 22:29:10 -0400

    Delete Triggers and functions.sql

--- Triggers and functions.sql
+++
@@ -1,6 +0,0 @@
-create trigger patients_GENDER 
-before INSERT 
-on 
-Patients 
-for each row 
-set Patients.total = patients.male1 + patients.male2 + patients.male3, patients.per = patients.total * 60 / 100;
